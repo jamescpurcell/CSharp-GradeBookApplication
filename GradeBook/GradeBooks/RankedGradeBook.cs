182a0fc5f58fbe1226f37f4b4ba1bdec43fd9d91
LAPTOP-VMUEI8B1\James <jamescpurcell@outlook.com>
2020-08-14 18:28:29 -0500

    1st attempt at module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,35 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count() < 5)
+            {
+                throw  new InvalidOperationException();
+            }
+
+            if (averageGrade >= 80.00)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= 60.00 && averageGrade < 80.00)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= 40.00 && averageGrade < 60.00)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= 20.00 && averageGrade < 40.00)
+            {
+                return 'D';
+            }
+            else 
+            {
+                return 'F';
+            }
         }
     }
 }
